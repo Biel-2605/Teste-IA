0a938473e982424b5fadb1e6904b8ad9eebc6e58
Gabriel Rotta <gabriel.rotta05@gmail.com>
2025-09-16 21:42:54 -0300

    Scripts Novos

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,12 @@
-using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public enum searchType
+{
+    BFS,
+    DFS
+}
 
 public class GameManager : MonoBehaviour
 {
@@ -12,6 +19,14 @@ public class GameManager : MonoBehaviour
     public GameObject node;
     public List<GameObject> nodes;
 
+    public List<GameObject> walls;
+    [HideInInspector]
+    public bool hasStart = false;
+    [HideInInspector]
+    public bool hasGoal = false;
+
+    [Header("Search Type")]
+    public searchType searchType = searchType.DFS;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,21 +40,24 @@ public class GameManager : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             HandleMouseClick();
-            Debug.Log("Clicou");
         }
     }
+     
 
     private void HandleMouseClick()
     {
+        // Cria um ray da câmera para a posição do mouse
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
+        // Verifica se o ray atingiu algum objeto
         if (Physics.Raycast(ray, out hit))
         {
+            // Verifica se o objeto atingido tem o componente Node
             Node nodeComponent = hit.collider.GetComponent<Node>();
             if (nodeComponent != null)
             {
-                nodeComponent.OnNodeClicked();
+                nodeComponent.OnNodeClicked();     
             }
         }
     }
--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -1,23 +1,68 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+public enum NodeType
+{
+    Floor,
+    Wall,
+    Start,
+    Goal
+}
 
 public class Node : MonoBehaviour
 {
     [Header("Materials")]
     public Material actualWay;
-    public Material blackList;
+    public Material blackListed;
     public Material finish;
     public Material floor;
     public Material possibleWay;
     public Material start;
     public Material wall;
 
+    [Header("Node Properties")]
+    public List<Node> neighbors = new List<Node>();
+    public NodeType nodeType = NodeType.Floor;
+
     bool isClicked = false;
 
     public void OnNodeClicked()
     {
-        Debug.Log($"Node clicado: {gameObject.name}");
+        if (nodeType != NodeType.Wall)
+        {
+            if (nodeType == NodeType.Start)
+            {
+                GetComponent<Renderer>().material = floor;
+                nodeType = NodeType.Floor;
+                GameObject.Find("GameManager").GetComponent<GameManager>().hasStart = false;
+                
+            }
+            else if (nodeType == NodeType.Goal)
+            {
+                GetComponent<Renderer>().material = floor;
+                nodeType = NodeType.Floor;
+                GameObject.Find("GameManager").GetComponent<GameManager>().hasGoal = false;
+            }
+            else
+            {
+                if (!GameObject.Find("GameManager").GetComponent<GameManager>().hasStart)
+                {
+                    GetComponent<Renderer>().material = start;
+                    nodeType = NodeType.Start;
+                    GameObject.Find("GameManager").GetComponent<GameManager>().hasStart = true;
+                }
+                else if (!GameObject.Find("GameManager").GetComponent<GameManager>().hasGoal)
+                {
+                    GetComponent<Renderer>().material = finish;
+                    nodeType = NodeType.Goal;
+                    GameObject.Find("GameManager").GetComponent<GameManager>().hasGoal = true;
+                }
+            }
+        }
 
-        if (!isClicked)
+        /*
+        if ( !isClicked )
         {
             GetComponent<Renderer>().material = start;
             isClicked = true;
@@ -27,5 +72,6 @@ public class Node : MonoBehaviour
             GetComponent<Renderer>().material = floor;
             isClicked = false;
         }
+        */
     }
 }
